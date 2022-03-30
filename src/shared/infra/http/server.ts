cf0f7ed810809915a5cb0f0698adfcfd9a54e2cf
Eduardo Oliveira <eduardooliveira256@gmail.com>
2022-03-30 18:15:19 -0300

    cors adding, yarn -> npm Procfile

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,14 @@ import 'reflect-metadata';
 import express from 'express';
 import { router } from './routes';
 import "../../../shared/container";
+import cors from 'cors';
 import "../typeorm";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(router);
 
